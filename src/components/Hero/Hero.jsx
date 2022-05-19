86b3892403a810e07c05d80957f6763727b09b5f
NaderNasr <nadernasr7@gmail.com>
2022-05-19 12:34:48 -0400

    added framer motion to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React from 'react';
 // import TextLoop from "react-text-loop";
 // import Circle from '../../assets/Circle.svg'
-import './styles.css'
+import './styles.css';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
     <>
+
       <div className='hero-container'>
         <div className='text-container'>
-          <p className='text'>Hi, my name is,</p>
-          <p className='text-name'>Nader Nasr.</p>
-          <p className='text-desc'>I build things for the web.</p>
-          <p className='text-summary'>
-            I’m a software engineer specializing in building
-            and designing exceptional digital experiences.
-          </p>
+          <motion.div
+            initial={{ opacity: 0, x: -300 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ ease: "easeOut", duration: 2 }}
+            animate={{ x: 0 }}
+          >
+            <p className='text'>Hi, my name is,</p>
+            <p className='text-name'>Nader Nasr.</p>
+            <p className='text-desc'>I build things for the web.</p>
+            <p className='text-summary'>
+              I’m a software engineer specializing in building
+              and designing exceptional digital experiences.
+            </p>
+          </motion.div>
+
           {/* <img className='circle-svg' src={Circle} alt='Circle' /> */}
         </div>
       </div>
