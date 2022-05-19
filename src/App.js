290cde65d8e0769d7b7a7d2df01741ecd359334e
NaderNasr <nadernasr7@gmail.com>
2022-05-19 01:11:18 -0400

    Added hero and about components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import About from "./components/About/About";
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Hero />
+      <About />
     </>
   );
 }
