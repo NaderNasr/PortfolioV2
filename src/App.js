78d68b6fa8d4e04446ce308c65b3daf6a5461980
NaderNasr <nadernasr7@gmail.com>
2022-05-19 12:33:09 -0400

    Added Experience, projects, contact components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Experience from "./components/Experience/Experience";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Projects from "./components/Porjects/Projects";
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       <Header />
       <Hero />
       <About />
+      <Experience />
+      <Projects />
+      <Contact />
     </>
   );
 }
