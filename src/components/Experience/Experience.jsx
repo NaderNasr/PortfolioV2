126fc152166d4fafed78875a25dc8a1c516b43d5
NaderNasr <nadernasr7@gmail.com>
2022-05-19 12:34:18 -0400

    incomplete experience component

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css'
+
+const Experience = () => {
+  return (
+    <div className='experience-container'>
+      <div className='experience-text-container'>
+        <p className='experience-text-header'>Experience<hr /></p>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Experience
