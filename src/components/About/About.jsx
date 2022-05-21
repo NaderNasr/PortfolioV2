97a2265058ddf31409c3699f76b83d439cf303a0
NaderNasr <nadernasr7@gmail.com>
2022-05-21 02:20:55 -0400

    about edit added dummy image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,32 +1,67 @@
 import React from 'react'
 import './styles.css'
+import { motion } from 'framer-motion'
+import { Col, Container, Row } from 'react-bootstrap'
+import Dummy from '../../assets/about-dummy.jpg'
 
 const About = () => {
   return (
-    <div className='about-container'>
-      <div className='about-text-container'>
-        <p className='about-text-header'>About Me <hr /></p>
-        <div>
-          <p className='about-text-desc'>
-            Hello! My name is Nader and I enjoy creating things
-            that live on the internet. My interest in web development
-            started back in 2012 when I decided to try editing custom
-            Tumblr themes — turns out hacking together a custom
-            reblog button taught me a lot about HTML & CSS!
-          </p>
-        </div>
-        <ul className='about-list'>
-          <li>React</li>
-          <li>Node.js</li>
-          <li>JavaScript (ES6+)</li>
-        </ul>
-        <ul className='about-list'>
-          <li>React</li>
-          <li>Node.js</li>
-          <li>JavaScript (ES6+)</li>
-        </ul>
+
+    <>
+
+      <div className='about-container'>
+        <Container>
+          <Row>
+            <Col>
+              <div className='about-text-container'>
+                <motion.div
+
+                  initial={{ opacity: 0, x: -300 }}
+                  transition={{ ease: "easeOut", duration: 1.2 }}
+                  animate={{ x: 0 }}
+                  whileInView={{ opacity: 1, y: -100 }}
+                >
+                  <p className='about-text-header'>About Me <hr /></p>
+                  <div>
+                    <p className='about-text-desc'>
+                      Hello! My name is Nader and I enjoy creating things
+                      that live on the internet. As a technical project manager, my interest in web development started back in 2017 whilst overlooking developers work, I began playing with JavaScript, html and css in my free time.
+                    </p>
+                    <br />
+                    <p className='about-text-desc'>
+                      Below are the technologies I am currently working with (and growings!):
+                    </p>
+                  </div>
+                  <br/>
+                  <Container>
+                    <Row>
+                      <Col>
+                        <ul className='about-list'>
+                          <li>React</li>
+                          <li>Node.js</li>
+                          <li>JavaScript (ES6+)</li>
+                        </ul>
+                      </Col>
+                      <Col>
+                        <ul className='about-list'>
+                          <li>React</li>
+                          <li>Node.js</li>
+                          <li>JavaScript (ES6+)</li>
+                        </ul>
+                      </Col>
+                      <Col></Col>
+                    </Row>
+                  </Container>
+                </motion.div>
+              </div>
+            </Col>
+            <Col>
+              <img alt='about' src={Dummy} style={{ width: '25vw', marginTop: '80px' }} />
+            </Col>
+          </Row>
+        </Container>
       </div>
-    </div>
+    </>
   )
 }
 
