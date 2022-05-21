935538bf03a8159e177d73ddd3608b335802e6ad
NaderNasr <nadernasr7@gmail.com>
2022-05-21 02:21:41 -0400

    fix animations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 // import TextLoop from "react-text-loop";
-// import Circle from '../../assets/Circle.svg'
+import Circle from '../../assets/LineCircleOrange.svg'
 import './styles.css';
 import { motion } from 'framer-motion';
+// import { FaReact } from 'react-icons/fa';
 
 const Hero = () => {
   return (
@@ -12,10 +13,9 @@ const Hero = () => {
         <div className='text-container'>
           <motion.div
             initial={{ opacity: 0, x: -300 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ ease: "easeOut", duration: 2 }}
+            transition={{ ease: "easeOut", duration: 1.2 }}
             animate={{ x: 0 }}
+            whileInView={{ opacity: 1, x: 0 }}
           >
             <p className='text'>Hi, my name is,</p>
             <p className='text-name'>Nader Nasr.</p>
@@ -25,8 +25,28 @@ const Hero = () => {
               and designing exceptional digital experiences.
             </p>
           </motion.div>
-
-          {/* <img className='circle-svg' src={Circle} alt='Circle' /> */}
+          {/* <div className='hero-icons'>
+            <FaReact size={50}/>
+          </div> */}
+          {/* <div className='hero-circle'>
+            <motion.div
+              // initial={{ opacity: 0, x: 700 }}
+              // transition={{ ease: "easeOut", duration: 2 }}
+              // animate={{ x: 0 }}
+              // whileInView={{ opacity: 1, x: 100 }}
+            >
+              <img className='circle-svg' src={Circle} alt='Circle' />
+            </motion.div>
+          </div>
+          <motion.div
+            initial={{ opacity: 0, x: 700 }}
+            transition={{ ease: "easeOut", duration: 2 }}
+            animate={{ x: -240 }}
+            whileInView={{ opacity: 1, x: 100 }}
+            style={{ overflow: 'hidden' }}
+          >
+            <img className='circle-svg' src={Circle} alt='Circle' />
+          </motion.div> */}
         </div>
       </div>
     </>
