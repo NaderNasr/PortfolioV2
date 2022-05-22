af87bb4057a2af52c85bc6ca8878c4759a166f54
NaderNasr <nadernasr7@gmail.com>
2022-05-22 17:50:35 -0400

    project data

---
+++ src/components/ProjectData/Data.jsx
@@ -0,0 +1,94 @@
+
+const Data = [
+
+  // {
+  //   id: 6,
+  //   number: '08',
+  //   title: 'The Book Cabin',
+  //   text: 'Coming soon',
+  //   badge: ['AWS', 'Amplify', 'Lambda', 'S3 Bucket', 'ReactJS', 'CSS', '...'],
+  //   image: 'https://images.unsplash.com/photo-1541661538396-53ba2d051eed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80',
+  //   GitHub: 'Coming Soon',
+  //   link: '',
+  //   video: ''
+  // },
+  {
+    id: 0,
+    number: '07',
+    title: 'Password KeepR',
+    text: `Password Keeper is a password manager that stores encrypted passwords online.`,
+    badge: ['HTML', 'CSS', 'Javascript', 'jQuery', 'AJAX', 'NodeJS', 'Express'],
+    image: 'https://images.unsplash.com/photo-1565619624098-cf4168a7cd9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
+    GitHub: 'Github',
+    link: 'https://github.com/NaderNasr/PasswordKeepR',
+    video: 'https://youtu.be/xp5xNIXyyqs'
+  },
+  {
+    id: 1,
+    number: '06',
+    title: 'Tweeter',
+    text: `A single page twitter clone application.`,
+    badge: ['HTML', 'CSS', 'Javascript', 'jQuery', 'AJAX', 'NodeJS', 'Express'],
+    image: `https://images.unsplash.com/photo-1540643587686-bc6cbdecf5e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80`,
+    GitHub: 'GitHub',
+    link: 'https://github.com/NaderNasr/Tweeter',
+    video: 'https://youtu.be/Pyh8Yygbgy0'
+  },
+  {
+    id: 2,
+    number: '05',
+    title: 'TinyApp',
+    text: `A URL shortening service and a link management platform.`,
+    badge: ['Node', 'Express', 'EJS', 'Bcrypt'],
+    image: 'https://images.unsplash.com/photo-1602345397613-0934a8812d23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=984&q=80',
+    GitHub: 'GitHub',
+    link: 'https://github.com/NaderNasr/tinyApp',
+    video: 'https://youtu.be/5h9vGvBZAbA'
+  },
+  {
+    id: 3,
+    number: '04',
+    title: 'Interview Scheduler',
+    text: "React application that allows users to book and cancel interview in real time with an API server. ",
+    badge: ['ReactJS', 'PostgreSQL', 'HTML', 'SASS', 'Jest', 'Axios', 'Storybook', 'Cypress'],
+    image: 'https://images.unsplash.com/photo-1546349851-64285be8e9fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    GitHub: 'GitHub',
+    link: 'https://github.com/NaderNasr/Interview-Scheduler',
+    video: 'https://youtu.be/ARUWl38eFFc'
+  },
+  {
+    id: 4,
+    number: '03',
+    title: 'Portal Mixed Reality',
+    text: "React Native educational application that allows users to visually learn through augmented and virtual reality ",
+    badge: ['React Native', 'AWS', 'HTML', 'CSS', 'ReactViro'],
+    image: 'https://images.unsplash.com/photo-1626553683558-dd8dc97e40a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    GitHub: 'GitHub',
+    link: 'https://github.com/GettCreative/PortalMR',
+    video: 'https://www.youtube.com/watch?v=hP1WnRXPVSY'
+  },
+  {
+    id: 5,
+    number: '02',
+    title: 'AR E-Commerce',
+    text: 'An E-commerce application that utilizes augmented reality for art, and wall decorations.',
+    badge: ['ReactJS', 'PostgreSQL', 'HTML', 'CSS', 'Material UI', 'Axios', 'CommerceJs', 'ThreeJS'],
+    image: 'https://images.unsplash.com/photo-1604871000636-074fa5117945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    GitHub: 'Github',
+    link: 'https://github.com/NaderNasr/art',
+    video: 'https://youtu.be/NsWg0Tryemg'
+  },
+  {
+    id: 7,
+    number: '01',
+    title: 'SvelteFlix',
+    text: 'A Movie database created with Svelte',
+    badge: ['Svelte', 'CSS', 'TMDB API'],
+    image: 'https://images.unsplash.com/photo-1537989036885-80b4d200958d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+    GitHub: 'Github',
+    link: 'https://github.com/NaderNasr/Svelte-Movie-DB',
+    site: 'https://svelteflix.netlify.app/'
+  }
+]
+
+export default Data;
