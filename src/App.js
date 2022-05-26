30381415b8625c5bec388864db633a41a2f0f8c8
NaderNasr <nadernasr7@gmail.com>
2022-05-26 13:27:04 -0400

    loading anim

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
+import { useEffect, useState } from 'react';
 import ReactGA from 'react-ga';
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-import Experience from "./components/Experience/Experience";
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import Project from "./components/Projects/ProjectGrid/Project";
-
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Experience from './components/Experience/Experience';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Project from './components/Projects/ProjectGrid/Project';
+import Loading from './components/Loading/Loading';
 
 const App = () => {
-
-  const TRACKING_ID = "UA-163447962-1";
+  const TRACKING_ID = 'UA-163447962-1';
   ReactGA.initialize(TRACKING_ID);
+
+  const [loading, setLoading] = useState(1000);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(0);
+    }, loading);
+  }, [loading]);
 
   return (
     <>
-      <Header />
-      <Hero />
-      <About />
-      <Experience />
-      <Project />
-      <Contact />
+      {loading ? (
+        <Loading/>
+      ) : (
+        <>
+          <Header />
+          <Hero />
+          <About />
+          <Experience />
+          <Project />
+          <Contact />
+        </>
+      )}
     </>
   );
-}
+};
 
 export default App;
