0cc78427dcca43289a5e5fa7f616457c3a1e692d
NaderNasr <nadernasr7@gmail.com>
2023-11-19 13:19:47 +0300

    react-GA

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ReactGA from 'react-ga4';
+import ReactGA from 'react-ga';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './../src/components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
